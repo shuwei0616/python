ffe5ba904292da52057c311a8f4f6f772ca4489e
shuwei0616 <curry134679@gmail.com>
2025-06-12 12:05:23 +0800

    init

---
+++ BMI.py
@@ -0,0 +1,8 @@
+h = float(input('請輸入身高(cm)：'))/100
+# 使用 float 轉換成浮點數後除以 100 ( 因為身高可能會有小數點 )
+
+w = float(input('請輸入體重(kg)：'))
+# 使用 float 轉換成浮點數 ( 因為體重可能會有小數點 )
+
+bmi = w/(h*h)                           
+print(f'你的 BMI 數值為：{bmi}')    
